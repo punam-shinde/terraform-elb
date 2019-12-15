c37be94432517237379903916943080ad8ea3ff2
punam-shinde <54739097+punam-shinde@users.noreply.github.com>
2019-12-15 18:43:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "AKIA5FUVXMJNF3BU36VN"
-  secret_key = "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"
+  access_key = "************"
+  secret_key = "**********************"
 }
